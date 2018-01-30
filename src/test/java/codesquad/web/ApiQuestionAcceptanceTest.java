4fb59d4fbd6bbe29385efbda9da195cc2c558eb4
hy43 <hyde0401@gmail.com>
2018-01-30 22:41:03 +0900

    Question Api에 대한 테스트 코드 작성

---
+++ src/test/java/codesquad/web/ApiQuestionAcceptanceTest.java
@@ -0,0 +1,40 @@
+package codesquad.web;
+
+import codesquad.domain.User;
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+public class ApiQuestionAcceptanceTest {
+    @Test
+    public void 질문_목록_보기() throws Exception {
+
+    }
+
+    @Test
+    public void 질문_상세_보기() throws Exception {
+
+    }
+
+    @Test
+    public void 질문_등록() throws Exception {
+
+    }
+
+    @Test
+    public void 질문_수정_login() throws  Exception {
+
+    }
+
+    @Test
+    public void 질문_삭제_login() throws  Exception {
+
+    }
+}
