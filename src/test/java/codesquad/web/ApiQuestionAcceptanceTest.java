26cc5bb281e360a0752aa789b339566e08df60cc
hy43 <hyde0401@gmail.com>
2018-02-01 13:01:04 +0900

    feat(step3) : 질문생성/수정/삭제 테스트 및 코드 작성

--- src/test/java/codesquad/web/ApiQuestionAcceptanceTest.java
+++ src/test/java/codesquad/web/ApiQuestionAcceptanceTest.java
@@ -1,19 +1,13 @@
 package codesquad.web;
 
-import codesquad.domain.Question;
 import codesquad.domain.User;
 import codesquad.dto.QuestionDto;
-import codesquad.dto.UserDto;
 import org.junit.Test;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class ApiQuestionAcceptanceTest extends AcceptanceTest {
@@ -39,14 +33,23 @@ public class ApiQuestionAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void 질문_수정_login() throws  Exception {
+        long id = 1;
+        User loginUser = defaultUser();
+        QuestionDto newQuestion = createQuestionDto();
+        basicAuthTemplate(loginUser).put(String.format("/api/question/%d",id), newQuestion, String.class);
 
+        QuestionDto updatedQuestion = template().getForObject(String.format("/api/question/%d",id), QuestionDto.class);
+        assertThat(updatedQuestion.getTitle(), is(newQuestion.getTitle()));
+        assertThat(updatedQuestion.getContents(), is(newQuestion.getContents()));
     }
 
     @Test
     public void 질문_삭제_login() throws  Exception {
         User loginUser = defaultUser();
         long id = 1;
-        ResponseEntity<String> response = basicAuthTemplate(loginUser).postForEntity(String.format("/api/question/%d", id), id, String.class);
-        assertThat(response.getStatusCode(), is(HttpStatus.FOUND));
+        template().delete(String.format("/api/question/%d", id), loginUser);
+
+        ResponseEntity<String> response = template().getForEntity(String.format("/api/question/%d", id), String.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
     }
 }
